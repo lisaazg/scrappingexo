d4955ce064064afa64ae2d332553e5847efbf60d
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-21 22:55:30 +0200

    Create crypto_spec.rb

---
+++ spec/crypto_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../lib/00_crypto.rb'
+
+describe "the crypto_scrapper method" do
+  it "should not return nil" do
+    expect(get_cryptocurrencies_values).not_to be_nil
+  end
+  
+  it "should contain more than 99 pairs (== at least 100)" do
+    expect(get_cryptocurrencies_values.length).to be > 99
+  end
+end
