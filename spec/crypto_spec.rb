3ba6714aa6a906305dd206c6c1c3b1712f431359
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-22 08:07:13 +0200

    Update crypto_spec.rb

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,6 @@
 require_relative '../lib/00_crypto.rb'
 
-describe "the crypto_scrapper method" do
+describe "the crypto method" do
   it "should not return nil" do
     expect(get_cryptocurrencies_values).not_to be_nil
   end
