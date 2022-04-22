1fc2e50a83b60e0ebab90882878bc6cdb12306e9
lisaazg <62955237+lisaazg@users.noreply.github.com>
2022-04-22 08:54:42 +0200

    Update crypto_spec.rb

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -4,8 +4,4 @@ describe "the crypto method" do
   it "should not return nil" do
     expect(get_cryptocurrencies_values).not_to be_nil
   end
-  
-  it "should contain more than 99 pairs (== at least 100)" do
-    expect(get_cryptocurrencies_values.length).to be > 99
-  end
 end
